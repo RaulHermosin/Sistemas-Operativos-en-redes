b69f5ed3a1c85c38757a77a4507beae9399fadc6
RaulHermosin <rezra7720@gmail.com>
2023-12-11 16:23:28 +0100

    Ejercicio python entregado

--- videojuego.py
+++ videojuego.py
@@ -1,4 +1,6 @@
 import random
+import pdb 
+
 class TratamientoFichero():
 
     def __init__(self, nombre_fichero):
@@ -21,7 +23,8 @@ class Partida:
     def __init__(self):
         self.resultado = random.randint(1, 10)
         self.acertar = False
-
+        self.nombre_jugador = ""
+        self.puntos_ganados = 0
   
 
 
@@ -40,20 +43,13 @@ class Partida:
                 print("Has conseguido un total de ", self.puntos_ganados, " puntos")
                 print("Porfavor, diganos tu nombre")
                 self.nombre_jugador = input()
-                
-
+                print(self.nombre_jugador)
                 break
 
             else:
                 print("Has fallado, vuelve a intentarlo")
                 self.intentos = self.intentos + 1
 
-
-
-            
-
-    def ranking(self):
-        self.read_file
         
 
 
@@ -61,24 +57,39 @@ class Menu:
     def __init__(self):
         self.opcion_elegida = None
         self.juego = Partida()
-
+        self.acabar_juego= False
+        self.f = TratamientoFichero("ranking.txt")
 
     def mostrar_menu(self):
-        print("1) Iniciar Partida")
-        print("2) Ranking")
-        print("3) Salir")
-        self.seleccionar()
+        while not self.acabar_juego:
+            print("1) Iniciar Partida")
+            print("2) Ranking")
+            print("3) Salir")
+            self.seleccionar()
 
     def seleccionar(self):
-        
         seleccion = input()
         if seleccion == "1":
            self.juego.jugar_partida()
+           self.escribir_solucion()
+           
+
+        if seleccion == "2":
+         self.f.read_file()
+
+
+        if seleccion == "3":
+            self.acabar_juego= True
+    
+    def escribir_solucion(self):
+        a_escribir = "¡" + self.juego.nombre_jugador + " ha obtenido " + str(self.juego.puntos_ganados) + " puntos!"
+ 
+        self.f.write_file(a_escribir)
+
             
 
 
 if __name__=='__main__':
     M1 = Menu()
     f = TratamientoFichero("ranking.txt")
-    f.write_file("¡", Partida.nombre_jugador, " ha obtenido ", Partida.puntos_ganados, " puntos!")
     M1.mostrar_menu()
