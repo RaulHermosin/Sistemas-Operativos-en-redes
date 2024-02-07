862d474c966a1ae41d26a2932a306ba63acbd1ea
RaulHermosin <rezra7720@gmail.com>
2024-02-07 15:59:00 +0100

    Trabajo Django a entregar

--- gestor/contraseña/views.py
+++ gestor/contraseña/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from django.http import JsonResponse
+import pdb
+from django.views.decorators.csrf import csrf_exempt
+import json
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("Porfavor, registre su usuario")
+
+@csrf_exempt
+def añadir(request):
+    
+    #response={
+    #    "Usuario": HttpResponse(usuario),
+    #    "Contraseña": HttpResponse(contraseña)
+    #}
+    pdb.set_trace()
+    string_body = request.body.decode('utf8').replace("'", '"')
+    body = json.loads(string_body)
+    body['usuario']
+    response =  {}     
+    return JsonResponse(response)
